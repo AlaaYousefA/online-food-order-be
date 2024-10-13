14036cec998caf4a5c5c5c3e7234fb2b3f784404
AlaaYousefA <loloyousef2622003@gmail.com>
2024-10-13 08:08:00 +0300

    remove unuse import

--- src/main/java/com/foodorder/backend/application/controllers/CartController.java
+++ src/main/java/com/foodorder/backend/application/controllers/CartController.java
@@ -1,16 +1,11 @@
 package com.foodorder.backend.application.controllers;
 
 import com.foodorder.backend.domain.services.CartService;
-import com.foodorder.backend.persistence.entity.OrdersEntity;
-import com.foodorder.backend.persistence.entity.SysUserEntity;
-import jakarta.persistence.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/cart")
